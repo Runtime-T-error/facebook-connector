09f56d3594b519aafcab151b6c5ed29eb1f19f70
djv3l <djv3l.dev@gmail.com>
2019-12-21 13:16:47 +0100

    Initial commit.

---
+++ src/main/java/org/raf/hackaton/facebook/FacebookConnectorApplication.java
@@ -0,0 +1,13 @@
+package org.raf.hackaton.facebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/raf/hackaton/facebook/FacebookConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raf.hackaton.facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
